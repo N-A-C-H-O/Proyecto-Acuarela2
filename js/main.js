8eadfda6b15db9667c3b22ed7bf4ee6c5817e907
Ignacio Kruchowski <ignakruchowski@gmail.com>
2022-08-17 16:37:18 -0300

    Modificación slider item y boton contacto

--- js/main.js
+++ js/main.js
@@ -21,3 +21,9 @@ const allSwiper = document.querySelectorAll('.swiper-slide');
 allSwiper.forEach(el => {
     el.style.height = `${ViewportHeight}px`;
 })
+
+const sliderItem3 = document.querySelector('.carousel-item__3');
+
+if (innerWidth >= 320 && innerWidth < 1024) {
+  sliderItem3.remove()
+}
